a04882ea515d9d2d53c7e31f4d1bf9fd3ea94081
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2023-09-12 00:43:33 +0700

    work on frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import Home from "./pages/Home";
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div>
+            <h1 className="text-red-500 text-center  font-khmer">សួស្តី</h1>
+        </div>
+     );
+}
+ 
+export default Home;
